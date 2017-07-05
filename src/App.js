f3530248ff696c5ca04d5fe4765bad4aeec1672e
Rahul2360 <rahulbansal2360@gmail.com>
2017-07-06 00:55:00 +0530

    add data through API

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css'
 import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
+  state = {
+    books:[]
+   }
+   componentDidMount() {
+     BooksAPI.getAll().then( (books)=>
+     this.setState({books}))
+   }
   render() {
     return (
       <div className="app">
-        <ListBooks />
+      <ListBooks books={this.state.books}/>
         </div>
-        )}}
-
-
+      )
+    }
+  }
 export default BooksApp
