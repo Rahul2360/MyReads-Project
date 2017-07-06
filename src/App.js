efd7c95717390f9fb3471c8eaa38d4fe873eb4b6
Rahul2360 <rahulbansal2360@gmail.com>
2017-07-06 11:22:22 +0530

    code reduced in app.js file

--- src/App.js
+++ src/App.js
@@ -4,17 +4,10 @@ import './App.css'
 import ListBooks from './ListBooks'
 
 class BooksApp extends React.Component {
-  state = {
-    books:[]
-   }
-   componentDidMount() {
-     BooksAPI.getAll().then( (books)=>
-     this.setState({books}))
-   }
   render() {
     return (
       <div className="app">
-      <ListBooks books={this.state.books}/>
+      <ListBooks/>
         </div>
       )
     }
--- src/ListBooks.js
+++ src/ListBooks.js
@@ -4,18 +4,39 @@ import './App.css'
 import BookDetails from './BookDetails'
 
 class ListBooks extends React.Component {
+/*  state ={
+    currentlyReading:[],
+    wantToRead:[],
+    read:[]
+  }*/
+  state = {
+    books:[]
+   }
+   componentDidMount() {
+     BooksAPI.getAll().then((books)=>
+      this.setState({books}))
+   }
   render() {
     return (
-      <div className="bookshelf">
-            <h2 className="bookshelf-title">My Reads</h2>
-            <div className="bookshelf-books">
-              <ol className="books-grid">
-              	{this.props.books.map(book => (
-	                <BookDetails book={book} key={book.id}/>
-                ))}
-              </ol>
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>My Reads</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+            <div className="bookshelf">
+                  <h2 className="bookshelf-title">Books</h2>
+                  <div className="bookshelf-books">
+                    <ol className="books-grid">
+                    	{this.state.books.map(book => (
+      	                <BookDetails book={book} key={book.id}/>
+                      ))}
+                    </ol>
+                  </div>
             </div>
-          </div>
+        </div>
+      </div>
+      </div>
     )
   }
 }
